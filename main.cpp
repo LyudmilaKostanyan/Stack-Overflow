058ac1c33f9ed54246d240f59536ae74c3cd1093
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-04-22 18:40:10 +0400

    Change print_stack_limit to return size and update main to use the returned value

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@
     #include <limits>
 #endif
 
-void print_stack_limit() {
+size_t print_stack_limit() {
+    size_t size;
 #if defined(_WIN32)
     MEMORY_BASIC_INFORMATION mbi;
     VirtualQuery(&mbi, &mbi, sizeof(mbi));
@@ -20,9 +21,9 @@ void print_stack_limit() {
 
     std::cout << "Estimated usable stack size (Windows): "
               << usableStack / 1024 << " KB\n";
+    size = usableStack;
 
 #elif defined(__unix__) || defined(__APPLE__)
-    // POSIX: use getrlimit
     struct rlimit rl;
     if (getrlimit(RLIMIT_STACK, &rl) == 0) {
         std::cout << std::fixed << std::setprecision(2);
@@ -30,6 +31,7 @@ void print_stack_limit() {
         std::cout << "Stack size limit (soft): "
                   << rl.rlim_cur / 1024 << " KB ("
                   << static_cast<double>(rl.rlim_cur) / (1024 * 1024) << " MB)\n";
+        size = rl.rlim_cur;
 
         if (rl.rlim_max == RLIM_INFINITY) {
             std::cout << "Stack size limit (hard): unlimited\n";
@@ -44,9 +46,17 @@ void print_stack_limit() {
 #else
     std::cout << "Unsupported platform.\n";
 #endif
+    return size;
 }
 
 int main() {
-    print_stack_limit();
+    size_t limit = print_stack_limit();
+    size_t safe_size = limit - (64 * 1024);
+
+    std::cout << "Trying to allocate " << safe_size / 1024 << " KB on stack...\n";
+
+    int array[safe_size / sizeof(int)];
+    std::cout << "Success.\n";
+
     return 0;
 }
